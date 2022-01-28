cab3889ba4e93d7797e10e250ce7bbda04624c68
whatsappjasbot <rssdevoloper@gmail.com>
2022-01-28 11:01:29 +0700

    first commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "# pokis" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/rssdevoloper/pokis.git
+git push -u origin main
